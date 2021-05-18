69b5e66ca3c0d32ebc2fbffe8bfba24d27b81ae4
Yonatan Lavie <yonatan.lavie89@gmail.com>
2021-05-18 08:27:03 +0300

    Connecting the Fron and Back Ends - add npm package whatwg-fetch - add proxy package.json - add components:    -  AddCommentForm    - CommentsList    - UpvoteSection - refactor ArticlePage

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import articles from './article-content'
-import ArticlesList from '../components/ArticlesList'
+import ArticlesList from '../components/ArticlesList';
+import CommentsList from '../components/CommentsList';
+import UpvotesSection from '../components/UpvotesSection';
+import AddCommentForm from '../components/AddCommentForm';
 import NotFoundPage from './NotFoundPage'
 
 const ArticlePage = ({match}) => {
     const name = match.params.name;
     const article = articles.find(article => article.name === name);
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: []});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            console.log(body);
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name]);
 
     if (!article) return <NotFoundPage />
 
@@ -14,9 +29,12 @@ const ArticlePage = ({match}) => {
     return (
         <>
             <h1>{article.title}</h1>
+            <UpvotesSection articleName={name} setArticleInfo={setArticleInfo} upvotes={articleInfo.upvotes} />
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
+            <CommentsList comments={articleInfo.comments} />
+            <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
             <h3>Other Articles</h3>
             <ArticlesList articles={otherArticles} />
         </>
